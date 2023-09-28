21108eddc9921ba2c0fff54dfcd1761fdb4c4826
MLuizaOliva <marialuizaolivasantos@gmail.com>
2023-09-27 21:23:00 -0300

    resol-ex-18

--- lista2/18.c
+++ lista2/18.c
@@ -1,7 +1,21 @@
+/*Idem ao exercício anterior, informe si for um triângulo equilátero.*/
+
 #include <stdio.h>
 
 int main(){
+    float x, y, z;
 
+    printf("informe o primeiro lado do triangulo:");
+    scanf("%f", &x);
+    printf("informe o segundo lado do triangulo:");
+    scanf("%f", &y);
+    printf ("informe o terceiro lado do triangulo:");
+    scanf("%f", &z);
+
+    if (x==y && y==z){
+        printf("triangulo equilatero");
+    }
+    else printf("nao eh equilatero");
 
     return 0;
 }
